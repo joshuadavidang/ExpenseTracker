9a388be63262a8df9f55acd6824ea1104bfdf449
Joshua David Ang <joshuadavidang@outlook.sg>
2022-06-18 21:22:03 +0800

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Joshua David Ang on 18/6/22.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
